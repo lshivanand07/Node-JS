0aefa9b91784da331386938336e21358ca3bbcab
Shivanandlamani <lshivanand07@gmail.com>
2026-07-16 14:37:29 +0530

    Update CORS

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
 import cors from "cors";
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express();
+
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
 
 app.use(cors({
   origin:  [
