b17ba34ffc493747b3fcd11faf59e57e89be824f
Shivanandlamani <lshivanand07@gmail.com>
2026-07-16 13:59:55 +0530

    update .env file

--- Backend/config/dbConnection.ts
+++ Backend/config/dbConnection.ts
@@ -9,6 +9,7 @@ if (!process.env.DB_HOST) {
 
 const db: Pool = mysql.createPool({
   host: process.env.DB_HOST || "",
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER || "",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "",
