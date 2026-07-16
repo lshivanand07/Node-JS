a313e6484730b6df5e2954c6b2b18160ae4e58f3
Shivanandlamani <lshivanand07@gmail.com>
2026-07-16 14:24:51 +0530

    Update CORS for Vercel

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -4,7 +4,10 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin:  [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL || '',
+    ],
   credentials: true,
 }));
 
